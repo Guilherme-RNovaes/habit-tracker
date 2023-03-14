34e06e6fbf6d937c8b81c8b58cdea83f4875acf4
guilh <guilherme00rogerio@gmail.com>
2023-03-13 22:35:09 -0300

    Implementação da função add

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-01', '01-02', '01-03'],
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
     
+    if (dayExists) {
+        alert('Dia já incluso 🔴')
+        return
+    }
+
+    alert('Adicionado com sucesso ✔️')
+    nlwSetup.addDay(today)
 }
+// const data = {
+//     run: ['01-01', '01-02', '01-03', '01-06'],
+//     water: ['01-01', '01-02', '01-04'],
+//     food: ['01-01', '01-04', '01-06'],
+// }
 
 nlwSetup.setData(data)
 nlwSetup.load()
